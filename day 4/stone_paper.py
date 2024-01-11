fb2fb0858f49d8b0d365b402f3b83cd8cd76c74c
sabir-kazi4 <156062527+sabir-kazi4@users.noreply.github.com>
2024-01-11 21:56:35 +0530

    Day 4 projects

---
+++ day 4/stone_paper.py
@@ -0,0 +1,35 @@
+from stone_paper_data import rock, paper, scissors
+
+# Write your code below this line 👇
+
+import random
+
+choices_1 = [rock, paper, scissors]
+choice_2 = ["rock", "paper", "scissors"]
+
+user = int(input("What do you choose? Type 0 for rock, 1 for paper, or for 2 scissors.\n"))
+
+print("Your choice : ")
+print(choice_2[user])
+print(choices_1[user])
+print("\n\n")
+
+computer = random.randint(0, 2)
+print("Computer choice : ")
+print(choice_2[computer])
+print(choices_1[computer])
+print("\n\n")
+
+if user < 3:
+    if user == 0 and computer == 2:
+        print("You Won!")
+    elif computer == 0 and user == 2:
+        print("you lose")
+    elif user > computer:
+        print("You Won!")
+    elif user < computer:
+        print("ou lose")
+    else:
+        print("its draw")
+else:
+    print("wrong choice by user you lose")
