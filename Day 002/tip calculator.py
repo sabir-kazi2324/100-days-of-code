b6034a96657e455a88f3946d1f7f82a3127b1c34
sabir-kazi4 <156062527+sabir-kazi4@users.noreply.github.com>
2024-01-13 14:26:44 +0530

    Tip calculator

--- Day 002/tip calculator.py
+++ Day 002/tip calculator.py
@@ -1,3 +1,5 @@
+# Tip calculator
+
 print("Welcome to the tip calculator.")
 total_bill = float(input("What was the total bill? $"))
 total_people = float(input("How many people to split the bill? "))
